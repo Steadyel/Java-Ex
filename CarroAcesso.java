62f81633a841eae70cf50f2df77d09ca161348a3
Steadyel <142850333+Steadyel@users.noreply.github.com>
2023-11-03 10:12:05 -0300

    Add files via upload

---
+++ CarroAcesso.java
@@ -0,0 +1,7 @@
+public class CarroAcesso {
+    public static void main(String[] args) {
+        var renegade = new Carro("Renegade", "Jeep", "Prata");
+        System.out.println(renegade.getMarca());
+
+    }
+}
